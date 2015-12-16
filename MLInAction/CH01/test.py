282be4f6a85ffa9a79dd135834cd1ec1f8f9f9b4
Jack <1270677763@qq.com>
2015-12-16 23:10:13 +0800

    首次正式使用github

---
+++ MLInAction/CH01/test.py
@@ -0,0 +1,15 @@
+from numpy import *
+
+if __name__ == '__main__':
+    #print(random.rand(4,4))
+    #矩阵
+    randMat=mat(random.rand(4,4))
+    #print(randMat)
+    #实现了矩阵求逆的运算
+    invRandMat=randMat.I
+    #print(invRandMat)
+    #矩阵和逆矩阵相乘(单位矩阵)
+    myEye=randMat*invRandMat
+    #print(myEye)
+    #处理计算机的结果误差
+    print(myEye-eye(4))
